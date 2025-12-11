506d37c8956e3589d3381df79fa2f12e7d25a92b
Henry Evans <nawazsarmast164@gmail.com>
2025-12-11 13:07:30 +0530

    feat: add global Toaster provider for notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "@/components/ui/sonner";
 import Index from "./pages/Index";
 import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
@@ -32,47 +33,50 @@ import ShopSettings from "./pages/Admin/Content/ShopSettings";
 // import InquiryView from "./pages/Admin/Inquiries/InquiryView";
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      {/* public routes */}
-      <Route path="/" element={<Index />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/product/:slug" element={<ProductDetail />} />
-      <Route path="/auth/callback" element={<AuthCallback />} />
+  <>
+    <BrowserRouter>
+      <Routes>
+        {/* public routes */}
+        <Route path="/" element={<Index />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/product/:slug" element={<ProductDetail />} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
 
-      {/* admin auth */}
-      <Route path="/admin/signin" element={<AdminSignin />} />
+        {/* admin auth */}
+        <Route path="/admin/signin" element={<AdminSignin />} />
 
-      {/* admin protected layout */}
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<ProductsList />} />
-        {/* Products */}
-        <Route path="products" element={<ProductsList />} />
-        <Route path="products/new" element={<ProductCreate />} />
-        <Route path="products/:id/edit" element={<ProductEdit />} />
-        <Route path="products/:id/media" element={<ProductMedia />} />
+        {/* admin protected layout */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<ProductsList />} />
+          {/* Products */}
+          <Route path="products" element={<ProductsList />} />
+          <Route path="products/new" element={<ProductCreate />} />
+          <Route path="products/:id/edit" element={<ProductEdit />} />
+          <Route path="products/:id/media" element={<ProductMedia />} />
 
-        {/* Orders */}
-        <Route path="orders" element={<OrdersList />} />
-        <Route path="orders/:id" element={<OrderView />} />
+          {/* Orders */}
+          <Route path="orders" element={<OrdersList />} />
+          <Route path="orders/:id" element={<OrderView />} />
 
-        {/* Content management */}
-        <Route path="content/brand" element={<BrandSettings />} />
-        <Route path="content/hero" element={<HeroSettings />} />
-        <Route path="content/gallery" element={<GallerySettings />} />
-        <Route path="content/footer" element={<FooterSettings />} />
-        <Route path="content/shop" element={<ShopSettings />} />
+          {/* Content management */}
+          <Route path="content/brand" element={<BrandSettings />} />
+          <Route path="content/hero" element={<HeroSettings />} />
+          <Route path="content/gallery" element={<GallerySettings />} />
+          <Route path="content/footer" element={<FooterSettings />} />
+          <Route path="content/shop" element={<ShopSettings />} />
 
-        {/* Inquiries */}
-        {/* <Route path="inquiries" element={<InquiriesList />} />
+          {/* Inquiries */}
+          {/* <Route path="inquiries" element={<InquiriesList />} />
         <Route path="inquiries/:id" element={<InquiryView />} /> */}
-      </Route>
+        </Route>
 
-      {/* fallback */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
+        {/* fallback */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+    <Toaster />
+  </>
 );
 
 export default App;
